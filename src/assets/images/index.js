dad469ecda836f5aad4cf0811aba2ee6ef37c4b6
black shadow <nirmalsujit541@gmail.com>
2024-07-31 19:15:33 +0530

    Final Portfolio ready

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -4,7 +4,7 @@ import shopify from './shopify.png'
 import starbucks from './starbucks.png'
 import tesla from './tesla.png'
 import logo from './logo.jpg'
-import socialSummer from './socialSummer1.png'
+import socialSummer1 from './socialSummer1.png'
 
 export {
     hero,
@@ -12,6 +12,6 @@ export {
     shopify,
     starbucks,
     tesla,
-    socialSummer,
+    socialSummer1,
     logo
 }
